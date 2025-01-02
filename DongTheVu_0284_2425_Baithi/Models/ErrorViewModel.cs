bb7516803782100280465701971b461e90294a3f
“VU2101 <dongthevu2k3@gmail.com>
2025-01-02 14:04:36 +0700

    CREATE_PROJECT

---
+++ DongTheVu_0284_2425_Baithi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DongTheVu_0284_2425_Baithi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
